5d4aaa854c997396922d7452f428fb7e74f76218
timlathy <tim.labushev@gmail.com>
2019-09-14 11:32:15 +0300

    [am lab1] rust python bindings

---
+++ Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/README.md
@@ -0,0 +1,12 @@
+## Setup
+
+```sh
+pip install virtualenv
+virtualenv applied-maths-lab1
+source applied-maths-lab1/bin/activate
+pip install maturin
+cd entropy
+maturin develop
+python3 # import entropy
+```
+
---
+++ Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/entropy/src/lib.rs
@@ -0,0 +1,13 @@
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+#[pymodule]
+fn entropy(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
+    Ok(())
+}
+
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
