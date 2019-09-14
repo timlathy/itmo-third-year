d89ed3beea9deda3641c7b7fa6bae1ed53e8eb83
timlathy <tim.labushev@gmail.com>
2019-09-14 22:44:59 +0300

    [am lab1] letter frequency

--- Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/entropy/src/lib.rs
+++ Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/entropy/src/lib.rs
@@ -1,13 +1,25 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use std::collections::HashMap;
 
 #[pymodule]
 fn entropy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(sum_as_string))?;
-    Ok(())
+    m.add_wrapped(wrap_pyfunction!(analyze_file))
 }
 
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn analyze_file(path: String) -> PyResult<HashMap<String, i32>> {
+    let text = std::fs::read_to_string(path)?;
+    let chars = text.chars().filter_map(|c| match c {
+        _ if c.is_ascii_alphabetic() => Some(c.to_ascii_lowercase()),
+        _ if c.is_ascii_punctuation() => Some('.'),
+        ' ' => Some(' '),
+        _ => None
+    });
+
+    let mut frequency = HashMap::new();
+    for c in chars {
+        *frequency.entry(c.to_string()).or_insert(0) += 1;
+    }
+    Ok(frequency)
 }
