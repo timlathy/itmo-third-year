5b61a6c10d1d20b32b3852189aaf9b3b74ffebec
timlathy <tim.labushev@gmail.com>
2019-09-16 09:41:59 +0300

    [am lab1] compute probability, create a jupyter notebook

--- Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/README.md
+++ Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/README.md
@@ -1,12 +1,30 @@
+## Prerequisites
+
+* Python 3.7
+* Nightly Rust toolchain
+
 ## Setup
 
 ```sh
 pip install virtualenv
 virtualenv applied-maths-lab1
 source applied-maths-lab1/bin/activate
-pip install maturin
+pip install maturin ipykernel
+ipython kernel install --user --name=applied-maths-lab1
+```
+
+## Development
+
+Building the native extension:
+
+```sh
 cd entropy
 maturin develop
-python3 # import entropy
+```
+
+Running the notebook:
+
+```sh
+jupyter notebook
 ```
 
--- Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/entropy/src/lib.rs
+++ Applied-Mathematics-5th-Term/Lab1-Shannon-Entropy/entropy/src/lib.rs
@@ -1,20 +1,22 @@
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use pyo3::wrap_pyfunction;
 use std::collections::HashMap;
 
 #[pymodule]
 fn entropy(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(analyze_file))
+    m.add_wrapped(wrap_pyfunction!(letter_frequencies))?;
+    m.add_wrapped(wrap_pyfunction!(frequencies_to_probabilities))
 }
 
 #[pyfunction]
-fn analyze_file(path: String) -> PyResult<HashMap<String, i32>> {
+fn letter_frequencies(path: &str) -> PyResult<HashMap<String, i32>> {
     let text = std::fs::read_to_string(path)?;
     let chars = text.chars().filter_map(|c| match c {
         _ if c.is_ascii_alphabetic() => Some(c.to_ascii_lowercase()),
         _ if c.is_ascii_punctuation() => Some('.'),
         ' ' => Some(' '),
-        _ => None
+        _ => None,
     });
 
     let mut frequency = HashMap::new();
@@ -22,4 +24,18 @@ fn analyze_file(path: String) -> PyResult<HashMap<String, i32>> {
         *frequency.entry(c.to_string()).or_insert(0) += 1;
     }
     Ok(frequency)
+}
+
+#[pyfunction]
+fn frequencies_to_probabilities(py: Python, freq_dict: PyObject) -> PyResult<PyObject> {
+    let freqs = freq_dict.cast_as::<PyDict>(py)?;
+    let sum = freqs.values().iter().fold(0.0, |sum, v| {
+        v.extract::<f32>().map(|freq| sum + freq).unwrap_or(sum)
+    });
+
+    let probabilities = PyDict::new(py);
+    for (k, freq) in freqs.iter() {
+        probabilities.set_item(k, freq.extract::<f32>()? / sum)?;
+    }
+    Ok(probabilities.to_object(py))
 }
