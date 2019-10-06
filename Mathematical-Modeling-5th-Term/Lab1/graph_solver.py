9f43953bc540de96dfd6981a798a8d19d6f60d0e
timlathy <tim.labushev@gmail.com>
2019-10-06 19:53:07 +0300

    [m lab1] wip solver

---
+++ Mathematical-Modeling-5th-Term/Lab1/graph_solver.py
@@ -0,0 +1,40 @@
+import numpy as np
+import sympy as sp
+
+class GraphSolver:
+    nodes = set()
+    adjacency = {}
+    symbols = {
+        'l': sp.Symbol('λ'),
+        'mu': sp.Symbol('μ')
+    }
+
+    def edge(self, a, b, **kwargs):
+        if a not in self.adjacency:
+            self.adjacency[a] = {}
+        self.adjacency[a][b] = kwargs['headlabel'] if 'headlabel' in kwargs else kwargs['label']
+        self.nodes.add(a)
+        self.nodes.add(b)
+
+    def make_equations(self, edge_equations):
+        ps = [sp.Symbol(f'p{i}') for i in range(len(self.nodes))]
+        self.symbols['p'] = ps
+
+        def cell(i_row, n_row, i_col, n_col):
+            if n_row in self.adjacency and n_col in self.adjacency[n_row]:
+                v = self.adjacency[n_row][n_col]
+                return edge_equations[v](ps[i_row])
+            return 0
+
+        def row(i_row, n_row):
+            cells = [cell(i_row, n_row, i_col, n_col) for i_col, n_col in enumerate(self.nodes)]
+            cells[i_row] = -1 * sum(cells)
+            return cells
+
+        cols = np.transpose([row(i_row, n_row) for i_row, n_row in enumerate(self.nodes)])
+        return [sp.Eq(sum(col), 0) for col in cols] + [sp.Eq(sum(ps), 1)]
+
+    def solve(self, equations, l, mu):
+        solution = sp.solve(equations, self.symbols['p'])
+        return {p: v.subs([(self.symbols['l'], l), (self.symbols['mu'], mu)]) for p, v in solution.items()}
+
