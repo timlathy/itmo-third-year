e238665c71c23069a918f847ae411a0e1dd71a8b
timlathy <tim.labushev@gmail.com>
2019-10-22 11:38:03 +0300

    [am lab3] specify encoding precision in bits

--- Applied-Mathematics-5th-Term/Lab3-Arithmetic-Coding/entry.rkt
+++ Applied-Mathematics-5th-Term/Lab3-Arithmetic-Coding/entry.rkt
@@ -1,10 +1,30 @@
 #lang racket
 
-(require "coder.rkt")
+(require math/bigfloat "coder.rkt")
 
-(define input "Fly me to the moon, let me play among the stars, and let me see what spring is like on Jupyter and Mars")
-(define codetable (make-code-table input))
-(define input-encoded (encode input codetable))
-(define input-decoded (decode input-encoded (string-length input) codetable))
-(display (~a "Encoded: " input-encoded "\n"))
-(display (~a "Decoded: " input-decoded "\n"))
+(define (charlist-mismatch-index lst1 lst2 [index 0])
+  (if (equal? (car lst1) (car lst2))
+    (charlist-mismatch-index (cdr lst1) (cdr lst2) (add1 index))
+    index))
+
+(define (do-code [input "Fly me to the moon, let me play among the stars, and let me see what spring is like on Jupyter and Mars"]
+                 [bits 53])
+  (bf-precision bits)
+  (define codetable (make-code-table input))
+  (define encoded-precise (encode input codetable))
+  (define encoded-trunc (bf encoded-precise))
+  (define decoded (decode (bigfloat->rational encoded-trunc) (string-length input) codetable))
+  (display (~a "Encoded: " (bigfloat->string encoded-trunc) "\n"))
+  (display (~a "Decoded: " decoded "\n"))
+  (display (~a "Encoding precision: " (charlist-mismatch-index (string->list input) (string->list decoded)) "\n")))
+
+(define (main)
+  (display "Enter the text to encode:\n")
+  (define input (read-line))
+  (display "Enter desired precision (in bits, 53 for double precision)\n")
+  (define bits (read))
+  (cond
+    [(not (integer? bits)) (display "Expected a number\n")]
+    [else (do-code input bits)]))
+
+(main)
