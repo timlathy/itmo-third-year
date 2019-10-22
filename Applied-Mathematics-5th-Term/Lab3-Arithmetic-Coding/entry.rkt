1d3a8c226e41b2ef8b4a5e2f8650f01785d4bf97
timlathy <tim.labushev@gmail.com>
2019-10-22 12:19:35 +0300

    [am lab3] compression ratio

--- Applied-Mathematics-5th-Term/Lab3-Arithmetic-Coding/entry.rkt
+++ Applied-Mathematics-5th-Term/Lab3-Arithmetic-Coding/entry.rkt
@@ -3,20 +3,27 @@
 (require math/bigfloat "coder.rkt")
 
 (define (charlist-mismatch-index lst1 lst2 [index 0])
-  (if (equal? (car lst1) (car lst2))
-    (charlist-mismatch-index (cdr lst1) (cdr lst2) (add1 index))
-    index))
+  (cond
+    [(and (not (empty? lst1)) (equal? (car lst1) (car lst2)))
+     (charlist-mismatch-index (cdr lst1) (cdr lst2) (add1 index))]
+    [else index]))
 
-(define (do-code [input "Fly me to the moon, let me play among the stars, and let me see what spring is like on Jupyter and Mars"]
-                 [bits 53])
+(define (do-code input bits)
   (bf-precision bits)
+
   (define codetable (make-code-table input))
   (define encoded-precise (encode input codetable))
   (define encoded-trunc (bf encoded-precise))
   (define decoded (decode (bigfloat->rational encoded-trunc) (string-length input) codetable))
+
+  (define precision (charlist-mismatch-index (string->list input) (string->list decoded)))
+  (define input-bits (* 8 (bytes-length (string->bytes/utf-8 input))))
+  (define compression-ratio (exact->inexact (* 100 (/ bits input-bits))))
+
   (display (~a "Encoded: " (bigfloat->string encoded-trunc) "\n"))
   (display (~a "Decoded: " decoded "\n"))
-  (display (~a "Encoding precision: " (charlist-mismatch-index (string->list input) (string->list decoded)) "\n")))
+  (display (~a "Encoding precision: " precision "\n"))
+  (display (~a "Compression ratio: " compression-ratio "%\n")))
 
 (define (main)
   (display "Enter the text to encode:\n")
