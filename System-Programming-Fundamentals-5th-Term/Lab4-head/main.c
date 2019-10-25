314a65a8a79d2b15bd516a7a460a52d6311e5265
timlathy <tim.labushev@gmail.com>
2019-10-25 14:26:00 +0300

    [spf lab4] replace fprintf with manual calls to write()

--- System-Programming-Fundamentals-5th-Term/Lab4-head/main.c
+++ System-Programming-Fundamentals-5th-Term/Lab4-head/main.c
@@ -1,11 +1,11 @@
 #define _POSIX_C_SOURCE 2
 #include <unistd.h>
 
-#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <stdarg.h>
 
 #define BUF_SIZE 4096
 
@@ -31,35 +31,42 @@ bool try_parse_uint(const char* str, unsigned int* val) {
   return endptr != str && *endptr == '\0';
 }
 
+// A fprintf-like wrapper over write().
+// Rationale: the assignment requires we only use raw syscalls to perform IO.
+void fdprintf(int fd, const char* fmt, ...) {
+  #define MSG_BUF_SIZE 512
+  char buf[MSG_BUF_SIZE];
+  va_list args;
+  va_start(args, fmt);
+  int msg_len = vsnprintf(buf, MSG_BUF_SIZE, fmt, args);
+  va_end(args);
+  write(fd, buf, msg_len < MSG_BUF_SIZE ? msg_len : MSG_BUF_SIZE);
+}
+
 void handle_filename_args(int argc, char** argv, int num_lines) {
   // According to POSIX,
   // If multiple file operands are specified, head shall precede the output for each with the header:
   // "\n==> %s <==\n", <pathname>
   // except that the first header written shall not include the initial <newline>.
-  #define HEADER_START "==> "
-  #define HEADER_END " <==\n"
-  #define HEADER_STDIN HEADER_START "standard input" HEADER_END
+  #define HEADER_STDIN "\n==> standard input <==\n"
+  #define HEADER_FILE_FMT "\n==> %s <==\n"
 
   bool first_header = true;
   do {
-    if (first_header)
-      first_header = false;
-    else
-      write(STDOUT_FILENO, "\n", sizeof("\n"));
+    int header_offset = first_header ? 1 : 0;
+    first_header = false;
 
     if (*argv[optind] == '-') {
-      write(STDOUT_FILENO, HEADER_STDIN, sizeof(HEADER_STDIN));
+      write(STDOUT_FILENO, HEADER_STDIN + header_offset, sizeof(HEADER_STDIN) - header_offset);
       print_lines_buffered(STDIN_FILENO, num_lines);
     }
     else {
       int fd = open(argv[optind], O_RDONLY);
       if (fd == -1) {
-        fprintf(stderr, "%s: Cannot open %s for reading", argv[0], argv[optind]);
+        fdprintf(STDERR_FILENO, "%s: Cannot open %s for reading", argv[0], argv[optind]);
       }
       else {
-        write(STDOUT_FILENO, HEADER_START, sizeof(HEADER_START));
-        write(STDOUT_FILENO, argv[optind], strlen(argv[optind]));
-        write(STDOUT_FILENO, HEADER_END, sizeof(HEADER_END));
+        fdprintf(STDOUT_FILENO, HEADER_FILE_FMT + header_offset, argv[optind]);
         print_lines_buffered(fd, num_lines);
         close(fd);
       }
@@ -76,8 +83,8 @@ int main(int argc, char** argv) {
     switch (c) {
       case 'n':
         if (!try_parse_uint(optarg, &num_lines)) {
-            fprintf(stderr, "%s: invalid number of lines: '%s'\n", argv[0], optarg);
-            return 1;
+          fdprintf(STDERR_FILENO, "%s: invalid number of lines: '%s'\n", argv[0], optarg);
+          return 1;
         }
     }
   }
