746f964770f34408b10077827d6ded4929718d7c
timlathy <tim.labushev@gmail.com>
2019-10-26 07:01:41 +0300

    [spf lab4] perl version

---
+++ System-Programming-Fundamentals-5th-Term/Lab4-head/head.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl -T
+
+# Run
+# cpan Getopt::Long
+# to fetch script dependencies
+
+use strict;
+use warnings qw(FATAL all);
+use Getopt::Long;
+
+my $num_lines = 10;
+
+GetOptions("n=i" => \$num_lines) or die("Usage: $0 [-n num-lines] [file...]\n");
+$num_lines > 0 or die("$0: invalid number of lines: '$num_lines'\n");
+
+my $printed = 0;
+my $first_file = 1;
+
+while (1) {
+  if ($printed == 0) {
+    if (not @ARGV and $first_file) {
+      # No arguments = don't print the header, read from stdin
+      undef $first_file;
+    }
+    elsif (@ARGV) {
+      my $leading_newline = $first_file ? "" : "\n";
+      my $filename = $ARGV[0] eq "-" ? "standard input" : $ARGV[0];
+      print "$leading_newline==> $filename <==\n";
+      undef $first_file;
+    }
+    else {
+      last;
+    }
+  }
+  if ($printed++ < $num_lines) {
+    my $line = <>;
+    print $line;
+  }
+  if ($printed == $num_lines or eof) {
+    $printed = 0;
+    close ARGV;
+  }
+}
