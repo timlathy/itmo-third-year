188fb6e640ac292ddb5edd51937a2337b9b05389
timlathy <tim.labushev@gmail.com>
2019-10-30 07:18:57 +0300

    [spf lab5] server: choose ipc mode based on args

--- System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsrv.c
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsrv.c
@@ -9,6 +9,8 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 
+typedef enum { M_UNDEF, M_SHMEM, M_MSGQ } ipc_mode_t;
+
 int run_sysv_shmem_server(pid_t pid, uid_t uid, gid_t gid) {
   errno = 0;
 
@@ -33,7 +35,28 @@ int run_sysv_shmem_server(pid_t pid, uid_t uid, gid_t gid) {
   return 0;
 }
 
+int run_sysv_msgq_server(pid_t pid, uid_t uid, gid_t gid) {
+  return 0;
+}
+
 int main(int argc, char** argv) {
+  #define USAGE "Usage: %s mode, where mode is either of:\n"\
+    "  -s: use System V shared memory\n"\
+    "  -q: use System V message queue\n"
+
+  int c;
+  ipc_mode_t mode = M_UNDEF;
+  while ((c = getopt(argc, argv, "sq")) != EOF) {
+    switch (c) {
+      case 's': mode = M_SHMEM; break;
+      case 'q': mode = M_MSGQ; break;
+    }
+  }
+  if (mode == M_UNDEF) {
+    fprintf(stderr, USAGE, argv[0]);
+    return 1;
+  }
+
   pid_t pid = getpid();
   uid_t uid = getuid();
   gid_t gid = getgid();
@@ -41,5 +64,9 @@ int main(int argc, char** argv) {
   printf("Started a server with pid=%jd, uid=%jd, gid=%jd\n",
       (intmax_t) pid, (intmax_t) uid, (intmax_t) gid);
 
-  return run_sysv_shmem_server(pid, uid, gid);
+  switch (mode) {
+    case M_SHMEM: return run_sysv_shmem_server(pid, uid, gid);
+    case M_MSGQ: return run_sysv_msgq_server(pid, uid, gid);
+    case M_UNDEF: return 1; // unreachable
+  }
 }
