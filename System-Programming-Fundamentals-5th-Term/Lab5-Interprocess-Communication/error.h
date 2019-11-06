28d06f56e92b15604873b21c280d6a2f4701d273
timlathy <tim.labushev@gmail.com>
2019-11-06 14:37:42 +0300

    [spf lab5] pthread mutex: looks good, doesn't work

--- System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/error.h
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/error.h
@@ -4,14 +4,27 @@
 #include <string.h>
 #include <stdio.h>
 
-#define CHK_ERRNO(expr) GEN_CHK_ERRNO(expr, int)
-#define T_CHK_ERRNO(expr) GEN_CHK_ERRNO(expr, void*)
+#define CHK_ERRNO(expr) GEN_CHK_ERRNO(expr, return 1)
+#define T_CHK_ERRNO(expr) GEN_CHK_ERRNO(expr, pthread_exit((void*)1))
 
-#define GEN_CHK_ERRNO(expr, ty) do {\
+#define GEN_CHK_ERRNO(expr, err_ret_expr) do {\
   expr;\
   if (errno != 0) {\
     fprintf(stderr, "%s:%d: Encountered an error, %s\n",\
         __FILE__, __LINE__, strerror(errno));\
-    return (ty)1;\
+    err_ret_expr;\
   }\
 } while (0)
+
+#define ERR_RET(expr) GEN_ERR_RET(expr, return 1)
+#define T_ERR_RET(expr) GEN_ERR_RET(expr, pthread_exit((void*)1))
+
+#define GEN_ERR_RET(expr, err_ret_expr) do {\
+  int __ret__ = expr;\
+  if (__ret__ != 0) {\
+    fprintf(stderr, "%s:%d: Encountered an error, %s\n",\
+        __FILE__, __LINE__, strerror(__ret__));\
+    err_ret_expr;\
+  }\
+} while (0)
+
---
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/pthreadarray.c
@@ -0,0 +1,60 @@
+#include "error.h"
+#include <pthread.h>
+
+volatile char letters[26];
+pthread_mutex_t letters_mutex;
+
+void* invert_case_thread(void* arg) {
+  while (1) {
+    T_ERR_RET(pthread_mutex_lock(&letters_mutex));
+
+    int case_incr = letters[0] == 'a' ? -32 : 32; // 'a' - 32 = 'A', 'A' + 32 = 'a'
+    for (int i = 0; i < 26; ++i)
+      letters[i] += case_incr;
+
+    T_ERR_RET(pthread_mutex_unlock(&letters_mutex));
+
+    sleep(1);
+  }
+}
+
+void* reverse_thread(void* arg) {
+  while (1) {
+    sleep(1);
+
+    T_ERR_RET(pthread_mutex_lock(&letters_mutex));
+
+    for (int i = 0; i < 26 / 2; ++i) {
+        int temp = letters[i];
+        letters[i] = letters[25 - i];
+        letters[25 - i] = temp;
+    }
+
+    T_ERR_RET(pthread_mutex_unlock(&letters_mutex));
+  }
+}
+
+int main(int argc, char** argv) {
+  for (int i = 0; i < 26; ++i)
+    letters[i] = 'a' + i;
+
+  pthread_t invcase_thrd, reverse_thrd;
+
+  ERR_RET(pthread_mutex_init(&letters_mutex, NULL));
+  ERR_RET(pthread_create(&invcase_thrd, NULL, invert_case_thread, NULL));
+  ERR_RET(pthread_create(&reverse_thrd, NULL, reverse_thread, NULL));
+
+  while (1) {
+    ERR_RET(pthread_mutex_lock(&letters_mutex));
+
+    for (int i = 0; i < 26; ++i)
+      printf("%c", letters[i]);
+    printf("\n");
+
+    ERR_RET(pthread_mutex_unlock(&letters_mutex));
+
+    sleep(1);
+  }
+
+  return 0;
+}
