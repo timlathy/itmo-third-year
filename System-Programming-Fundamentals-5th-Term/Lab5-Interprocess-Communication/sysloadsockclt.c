c21d358cb9cd00971b3936e759cf501abd72d8c6
timlathy <tim.labushev@gmail.com>
2019-11-08 00:56:19 +0300

    [spf lab5] unix domain socket client

---
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsockclt.c
@@ -0,0 +1,35 @@
+#define _DEFAULT_SOURCE
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include "error.h"
+#include "sysloadsock.h"
+
+int main(int argc, char** argv) {
+  int sockfd;
+  CHK_ERRNO(sockfd = socket(AF_UNIX, SOCK_STREAM, 0));
+
+  struct sockaddr_un sockaddr;
+  sockaddr.sun_family = AF_UNIX;
+  strncpy(sockaddr.sun_path, LAB_SOCK_PATH, sizeof(sockaddr.sun_path) - 1);
+  unsigned int sockaddr_len = sizeof(struct sockaddr_un);
+
+  CHK_ERRNO(connect(sockfd, (const struct sockaddr *) &sockaddr, sockaddr_len));
+
+  server_state_t state;
+  CHK_ERRNO(read(sockfd, &state, sizeof(server_state_t)));
+
+  printf("Connected to server with pid=%jd, uid=%jd, gid=%jd\n",
+      (intmax_t) state.pid, (intmax_t) state.uid, (intmax_t) state.gid);
+  printf("Load average:\n  1 minute: %.2lf\n  5 minutes: %.2lf\n  15 minutes: %.2lf\n",
+      state.loadavg[0], state.loadavg[1], state.loadavg[2]);
+
+  close(sockfd);
+
+  return 0;
+}
--- System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsocksrv.c
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsocksrv.c
@@ -22,8 +22,10 @@ int main(int argc, char** argv) {
   struct sockaddr_un sockaddr;
   sockaddr.sun_family = AF_UNIX;
   strncpy(sockaddr.sun_path, LAB_SOCK_PATH, sizeof(sockaddr.sun_path) - 1);
-  int sockaddr_len = sizeof(struct sockaddr_un);
+  unsigned int sockaddr_len = sizeof(struct sockaddr_un);
 
+  unlink(LAB_SOCK_PATH);
+  errno = 0;
   CHK_ERRNO(bind(sockfd, (const struct sockaddr *) &sockaddr, sockaddr_len));
   CHK_ERRNO(listen(sockfd, 0));
 
@@ -31,7 +33,16 @@ int main(int argc, char** argv) {
 
   while (1) {
     int cltfd;
-    CHK_ERRNO(cltfd = accept(sockfd, (struct sockaddr *) &sockaddr, sockaddr_len));
+    CHK_ERRNO(cltfd = accept(sockfd, (struct sockaddr*) &sockaddr, &sockaddr_len));
+
+    getloadavg(state.loadavg, 3);
+
+    write(cltfd, (const void*) &state, sizeof(server_state_t));
+    if (errno != 0)
+      fprintf(stderr, "Failed to send data to a client: %s\n", strerror(errno));
+
+    close(cltfd);
+    errno = 0;
   }
 
   return 0;
