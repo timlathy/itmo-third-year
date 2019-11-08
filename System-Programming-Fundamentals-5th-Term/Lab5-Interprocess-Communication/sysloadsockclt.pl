049666d9bed44a2b8e3248b395a0c4ceebfe7123
timlathy <tim.labushev@gmail.com>
2019-11-09 00:32:32 +0300

    [spf lab5] perl: code style

--- System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsockclt.pl
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsockclt.pl
@@ -5,14 +5,14 @@ use warnings qw(FATAL all);
 use IO::Socket::UNIX;
 
 use constant STATE_FMT => 'i<i<i<d<d<d<';
-use constant STATE_SIZE => length(pack(STATE_FMT, 0, 0, 0, 0, 0, 0));
+use constant STATE_SIZE => length pack(STATE_FMT, 0, 0, 0, 0, 0, 0);
 use constant LAB_SOCK_PATH => '/tmp/spf-lab5-sysload-sock';
 
 my $client = IO::Socket::UNIX->new(Type => SOCK_STREAM(), Peer => LAB_SOCK_PATH)
-  or die("Failed to connect to a socket: $!\n");
+  or die "Failed to connect to a socket: $!\n";
 
 my $state_buf = "";
-read($client, $state_buf, STATE_SIZE);
+read $client, $state_buf, STATE_SIZE;
 my ($spid, $suid, $sgid, $sl1, $sl5, $sl15) = unpack STATE_FMT, $state_buf;
 
 print "Connected to server with pid=$spid, uid=$suid, gid=$sgid\n";
--- System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsocksrv.pl
+++ System-Programming-Fundamentals-5th-Term/Lab5-Interprocess-Communication/sysloadsocksrv.pl
@@ -8,27 +8,27 @@ use English;
 use constant STATE_FMT => 'i<i<i<d<d<d<';
 use constant LAB_SOCK_PATH => '/tmp/spf-lab5-sysload-sock';
 
-unlink(LAB_SOCK_PATH);
+unlink LAB_SOCK_PATH;
 
 my $server = IO::Socket::UNIX->new(
   Type => SOCK_STREAM(), Local => LAB_SOCK_PATH, Listen => 1
-) or die("Failed to create a socket: $!\n");
+) or die "Failed to create a socket: $!\n";
 
 my $realgid = (split ' ', $GID)[0];
 print "Started a server with pid=$PID, uid=$UID, gids=$realgid\n";
 
 while (my $clt = $server->accept()) {
   my ($l1, $l5, $l15) = getloadavg();
-  my $state = pack(STATE_FMT, $PID, $UID, $realgid, $l1, $l5, $l15);
+  my $state = pack STATE_FMT, $PID, $UID, $realgid, $l1, $l5, $l15;
 
   $clt->print($state);
   $clt->close();
 }
 
 sub getloadavg {
-  $ENV{'PATH'} = '/usr/bin';
+  $ENV{'PATH'} = '/usr/bin'; # explicitly setting PATH is required for -T mode
   my $uptime = `uptime`;
-  $uptime =~ s/,/./g;
+  $uptime =~ s/,/./g; # crude handling for comma as a decimal separator
   $uptime =~ /load average: (\d+\.\d+)\. (\d+\.\d+)\. (\d+\.\d+)/;
   return ($1, $2, $3);
 }
