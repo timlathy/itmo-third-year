38e3aacc5f5d339e447f288c32b966d492287d86
timlathy <tim.labushev@gmail.com>
2019-11-11 21:10:55 +0300

    [am lab5] test single bit error

--- Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/coder-test.rkt
+++ Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/coder-test.rkt
@@ -10,6 +10,13 @@
         (define src (string->bit-vector data))
         (define encoded (encode-cc src))
         (define decoded (decode-cc encoded))
-        (check-equal? (bit-vector->string decoded) data)))))
+        (check-equal? (bit-vector->string decoded) data)))
+
+    (test-case "errors"
+      (define src (string->bit-vector "10011"))
+      (define encoded (encode-cc src))
+      (bit-vector-set! encoded 1 (not (bit-vector-ref encoded 1)))
+      (define decoded (decode-cc encoded))
+      (check-equal? decoded 'error))))
 
 (run-tests coder-tests)
