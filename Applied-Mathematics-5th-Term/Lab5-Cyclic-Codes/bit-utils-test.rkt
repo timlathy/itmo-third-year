4882cc64056fcebd2528ef494476fa6787683584
timlathy <tim.labushev@gmail.com>
2019-11-11 20:46:19 +0300

    [am lab5] utils: modulo 2 remainder

--- Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/bit-utils-test.rkt
+++ Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/bit-utils-test.rkt
@@ -4,11 +4,22 @@
 
 (define bit-utils-tests
   (test-suite "bit-utils.rkt tests"
-
     (test-case "resize-bit-vector creates a new bit vector"
       (define src (string->bit-vector "10011"))
       (define res (resize-bit-vector src 12))
       (check-equal? (bit-vector->string src) "10011")
-      (check-equal? (bit-vector->string res) "100110000000"))))
+      (check-equal? (bit-vector->string res) "100110000000"))
+
+    (test-case "module2-rem"
+      (define cases '(
+        ("10011" "10011" "00000")
+        ("100100000" "1101" "000000001")
+        ("100000001" "1101" "000000011")))
+      (for ([c (in-list cases)])
+        (match-define (list divident divisor expected) c)
+        (define res (modulo2-rem
+          (string->bit-vector divident)
+          (string->bit-vector divisor)))
+        (check-equal? (bit-vector->string res) expected)))))
 
 (run-tests bit-utils-tests)
--- Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/bit-utils.rkt
+++ Applied-Mathematics-5th-Term/Lab5-Cyclic-Codes/bit-utils.rkt
@@ -2,8 +2,22 @@
 
 (require data/bit-vector)
 
-(provide resize-bit-vector)
+(provide resize-bit-vector modulo2-rem)
 
 (define (resize-bit-vector src-vec new-size [fill #f])
   (for/bit-vector #:length new-size #:fill fill
     ([x (in-bit-vector src-vec)]) x))
+
+(define (modulo2-rem divident divisor)
+  (define result (bit-vector-copy divident))
+  (define (modulo2-rem-loop bit-i)
+    (for ([(d j) (in-indexed (in-bit-vector divisor))])
+      (define res-bit (bit-vector-ref result (+ bit-i j)))
+      (bit-vector-set! result (+ bit-i j) (xor res-bit d)))
+    (define (skip-zeroes i)
+      (cond
+        [(> i (- (bit-vector-length result) (bit-vector-length divisor))) result]
+        [(not (bit-vector-ref result i)) (skip-zeroes (add1 i))]
+        [else (modulo2-rem-loop i)]))
+    (skip-zeroes bit-i))
+  (modulo2-rem-loop 0))
