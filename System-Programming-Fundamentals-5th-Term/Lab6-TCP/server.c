e0e81082d5dce430636feeda54983e74fbf9befa
timlathy <tim.labushev@gmail.com>
2019-11-18 08:55:30 +0300

    [spf lab6] read plaintext request

--- System-Programming-Fundamentals-5th-Term/Lab6-TCP/server.c
+++ System-Programming-Fundamentals-5th-Term/Lab6-TCP/server.c
@@ -1,8 +1,43 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 #include "error.h"
+
+#define CLIENT_BUFSIZE 512
+
+// Protocol definition:
+//
+// <request> ::= <path> { <path> } CR LF
+// <path> ::= <non-empty string not including NUL or CR or LF> CR LF
+
+int handle_client(int fd) {
+  char* request = NULL;
+
+  char buf[CLIENT_BUFSIZE];
+  int bytes_read, request_len = 0;
+  while ((bytes_read = read(fd, buf, CLIENT_BUFSIZE)) > 0) {
+    request = realloc(request, request_len + bytes_read + 1);
+    memcpy(request + request_len, buf, bytes_read);
+    request_len += bytes_read;
+
+    if (request_len < 2) continue;
+    if (request[request_len - 2] == '\r' && request[request_len - 1] == '\n') {
+      if (request_len == 2) {
+        fprintf(stderr, "Malformed request: empty pathstr recieved\n");
+        return 1;
+      }
+      else if (request_len > 4 && request[request_len - 4] == '\r' && request[request_len - 3] == '\n') {
+        break;
+      }
+    }
+  }
+  request[request_len] = '\0';
+
+  printf("Received request: %s", request);
+  return 0;
+}
 
 int main(int argc, char** argv) {
   int sockfd;
@@ -28,6 +63,7 @@ int main(int argc, char** argv) {
     CHK_ERRNO(clientfd = accept(sockfd, NULL, NULL));
 
     printf("Client connected: %d\n", clientfd);
+    ERR_RET(handle_client(clientfd));
 
     CHK_ERRNO(close(clientfd));
   }
