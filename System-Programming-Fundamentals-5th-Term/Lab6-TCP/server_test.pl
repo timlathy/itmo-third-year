3227f0645ecfbce1deaadab26d747ef9fcc77570
timlathy <tim.labushev@gmail.com>
2019-11-18 16:43:57 +0300

    [spf lab6] server tests: create a function to send multiple requests

--- System-Programming-Fundamentals-5th-Term/Lab6-TCP/server_test.pl
+++ System-Programming-Fundamentals-5th-Term/Lab6-TCP/server_test.pl
@@ -6,22 +6,31 @@ use IO::Socket::INET;
 
 my $port = shift or die "Usage $0 port\n";
 
-my $clt = new IO::Socket::INET(PeerHost => 'localhost', PeerPort => $port, Proto => 'tcp')
-  or die "Failed to connect to the server: $!\n";
-
-$clt->send("/home/dir1\r\n/home/dir1/nested\r\n\r\n");
-
-my $response = "";
-while (1) {
-  $clt->recv(my $chunk, 1024);
-  if (scalar $chunk) {
-    $response .= $chunk;
-  }
-  else {
-    last
-  }
-}
-
-is $response, "Directory: [/home/dir1]\r\nDirectory: [/home/dir1/nested]\r\n\r\n";
+my $resp = send_request("/home/dir1\r\n/home/dir1/nested\r\n\r\n");
+is $resp, "Directory: [/home/dir1]\r\nDirectory: [/home/dir1/nested]\r\n\r\n";
 
 done_testing();
+
+sub send_request {
+  my $request = shift;
+
+  my $clt = new IO::Socket::INET(
+    PeerHost => 'localhost',
+    PeerPort => $port,
+    Proto => 'tcp'
+  ) or die "Failed to connect to the server: $!\n";
+
+  $clt->send($request);
+
+  my $response = "";
+  while (1) {
+    $clt->recv(my $chunk, 1024);
+    if (scalar $chunk) {
+      $response .= $chunk;
+    }
+    else {
+      last
+    }
+  }
+  $response
+}
