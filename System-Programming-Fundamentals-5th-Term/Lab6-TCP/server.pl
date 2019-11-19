df74a81d5ad24e930986f8e739e423c08243f05e
timlathy <tim.labushev@gmail.com>
2019-11-19 17:37:10 +0300

    [spf lab6] perl server (single-threaded for now)

---
+++ System-Programming-Fundamentals-5th-Term/Lab6-TCP/server.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings qw(FATAL all);
+use IO::Socket::INET;
+
+use constant USAGE => "Usage $0 port\n";
+my ($port) = shift =~ /(\d+)/ or die USAGE;
+
+my $server = IO::Socket::INET->new(
+  LocalPort => $port, Proto => 'tcp', Listen => SOMAXCONN
+) or die "Failed to start the server: $!\n";
+
+print "Listening on $port\n";
+
+while (my $clt = $server->accept()) {
+  my $request = "";
+  while (<$clt>) {
+    $request .= $_;
+    last if $request =~ /\r\n\r\n$/;
+  }
+
+  my @dirs = split /\r\n/, $request;
+
+  for my $dir (@dirs) {
+    if (opendir my $dirent, $dir) {
+      my @files = readdir $dirent;
+      closedir $dirent;
+
+      print $clt "Contents of $dir:\r\n" . join("\r\n", @files) . "\r\n\r\n";
+    }
+    else {
+      print $clt "Failed to open $dir: $!\r\n\r\n";
+    }
+  }
+
+  close $clt;
+}
