dd0e21ad5e1f464363805621d9dda9554b080ca8
timlathy <tim.labushev@gmail.com>
2020-04-23 19:24:46 +0300

    [at task3] d flip-flop circuit

--- Automata-Theory-6th-Term/Task3/Logic.hs
+++ Automata-Theory-6th-Term/Task3/Logic.hs
@@ -16,6 +16,16 @@ render (Not t) = "\\bar{" ++ render t ++ "}"
 render (X i) = "x_" ++ show i
 render (Q i) = "Q_" ++ show i
 
+renderNumericDnf :: BoolTerm -> String
+renderNumericDnf (Or ts) = intercalate " \\lor " $ renderNumericDnf <$> ts
+renderNumericDnf (And ts) = show $ binToDec $ bit <$> ts
+  where
+    bit (Not _) = 0
+    bit _ = 1
+    binToDec (0:bs) = binToDec bs
+    binToDec (1:bs) = 2^(length bs) + binToDec bs
+    binToDec [] = 0
+
 simplify :: BoolTerm -> BoolTerm
 simplify (And ts) = And $ simplify <$> filter (not. emptyTerm) ts
 simplify (Or ts) = Or $ simplify <$> filter (not. emptyTerm) ts
