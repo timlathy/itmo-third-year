8158233aa54440db7a2dd04a8d9237310be9ce9f
timlathy <tim.labushev@gmail.com>
2020-04-23 12:37:13 +0300

    [at task3] move table rendering to a separate module

---
+++ Automata-Theory-6th-Term/Task3/LatexTable.hs
@@ -0,0 +1,27 @@
+module LatexTable where
+
+import Data.List (intercalate)
+
+data TableHeader = RowHeader | ColHeader | NoHeader deriving (Eq)
+data TableCellEnvironment = DefaultCells | MathCells deriving (Eq)
+
+render :: TableHeader -> TableCellEnvironment -> [String] -> [[String]] -> String
+render h env cols rows =
+    "\\begin{tabular}{|*{" ++ show (length cols) ++ "}{c|}}\n\\hline\n" ++
+    joinRows (fmtCols (headerCell <$> cols) : (fmtCols <$> rows) ++ ["\\end{tabular}"])
+    where
+        headerCell c
+            | h == RowHeader = boldCell c
+            | otherwise      = c
+        fmtCols (c : cs)
+            | h == ColHeader = intercalate " & " (boldCell c : (cell <$> cs))
+            | otherwise      = intercalate " & " (cell <$> (c : cs))
+        joinRows = intercalate "\\\\\\hline\n"
+        boldCell c = "\\textbf{" ++ cell c ++ "}"
+        cell c
+            | env == MathCells = "$" ++ c ++ "$"
+            | otherwise        = c
+
+prependRowHeader :: [a] -> [[a]] -> [[a]]
+prependRowHeader (h:hs) (cs:css) = (h:cs) : prependRowHeader hs css
+prependRowHeader _ _ = []
